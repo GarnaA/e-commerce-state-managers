9d8104dc3c7a994ec7044c104958ea0044b78ce2
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2025-04-15 22:57:30 +0200

    add constants for cart actions

---
+++ src/redux/constants/Cartconstants.js
@@ -0,0 +1,8 @@
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+export const INCREASE_AMOUNT = 'INCREASE_AMOUNT'
+export const DECREASE_AMOUNT = 'DECREASE_AMOUNT'
+export const CLEAR_CART = 'CLEAR_CART'
+export const SET_ITEM_AMOUNT = 'ITEM_AMOUNT'
+export const SET_CART = 'CART'
+export const SET_TOTAL = 'TOTAL'
