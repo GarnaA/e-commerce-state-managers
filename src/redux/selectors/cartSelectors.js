5574c78c532cf5e819e7b05c71007d797ad7d7dc
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2025-04-18 11:20:23 +0200

    add cart, sidebar, product selectors

---
+++ src/redux/selectors/cartSelectors.js
@@ -0,0 +1,3 @@
+export const selectCartItems = (state) => state.cart.items;
+export const selectCartItemAmount = (state) => state.cart.itemAmount;
+export const selectCartTotal = (state) => state.cart.total;
---
+++ src/redux/selectors/sidebarSelectors.js
@@ -0,0 +1,1 @@
+export const selectSidebarIsOpen = (state) => state.sidebar.isOpen;
